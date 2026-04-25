e39238a8300ed0ff0e79658cbe76bc270372a1f1
Gus Yudha <gyudha497@gmail.com>
2026-04-26 01:49:58 +0800

    Tugas Pertemuan 7

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('menu');
+});
+
+Route::get('/pertemuan1', function () {
+    return view('pertemuan1');
+});
+Route::get('/pertemuan2', function () {
+    return view('pertemuan2');
+});
+Route::get('/pertemuan3', function () {
+    return view('pertemuan3');
+});
+Route::get('/pertemuan4', function () {
+    return view('pertemuan4');
+});
+Route::get('/pertemuan5', function () {
+    return view('pertemuan5');
+});
+Route::get('/pertemuan6', function () {
+    return view('pertemuan6');
+});
